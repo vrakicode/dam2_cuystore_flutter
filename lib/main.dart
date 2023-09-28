4c2cb15317cf64a445b9a9a5254412056945d2b6
Agustin Mamani <agusdmu1999@gmail.com>
2023-09-28 11:16:33 -0500

    conexion firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-void main() => runApp(const MyApp());
+import 'package:proyecto_flutter/firebase_options.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+} 
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,6 +24,7 @@ class MyApp extends StatelessWidget {
         ),
         body: const Center(
           child: Text('Hello World'),
+          
         ),
       ),
     );
